159592fa286ef6819ef88edbadd4b495a5d2c0fa
D3ryk <D3ryk@users.noreply.github.com>
2016-02-02 18:45:03 +0100

    Utilizing Composer autoloader + changes in dir struct

--- AbstractFactory/MessageFactory.php
+++
@@ -1,7 +0,0 @@
-<?php
-    interface MessageFactory {
-        public function createReceiver($name, $surname, $machine);
-        public function createSender($name, $surname, $machine);
-        public function createMessage($content);
-    }
-?>
