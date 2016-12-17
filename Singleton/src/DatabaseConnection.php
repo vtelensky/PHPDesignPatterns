62ceefb1493c91b829c93145370e62be82e65959
D3ryk <vaclav.telensky@seznam.cz>
2016-12-17 17:41:07 +0100

    Missing anotation in Singleton

--- Singleton/src/DatabaseConnection.php
+++ Singleton/src/DatabaseConnection.php
@@ -4,19 +4,28 @@ namespace Singleton;
 class DatabaseConnection
 implements Connection
 {
+    /** @var array  */
     private static $instances = array(
         'DB1' => null,
         'DB2' => null,
         'DB3' => null
     );
 
+    /** @var String **/
     private $dbName;
 
+    /** @param String $dbName */
     private function __construct($dbName)
     {
         $this->dbName = $dbName;
     }
 
+    /**
+     * @param String $dbName
+     *
+     * @return Connection
+     * @throws \Exception
+     */
     public static function getInstance($dbName)
     {
         if (!array_key_exists($dbName, self::$instances)) {
